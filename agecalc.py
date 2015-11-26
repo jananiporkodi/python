acab0924d35bc4d851422a6cbb22456efeffa96e
aspire <aspire@aspire891.aspiresys.com>
2015-11-26 19:33:39 +0530

    modify code to calculate age from DOB

--- agecalc.py
+++ agecalc.py
@@ -1,8 +1,11 @@
 import time
 import datetime
-dob = input("enter a date")
-a = datetime.datetime.strptime(dob, "%d-%m-%Y").date()
-print("DOB = ", a)
-today = datetime.date.today()
-print("Age = ", end="")
-print(today.year - a.year)
+dob = input("enter a date (DD-MM-YYYY)")
+try:
+	a = datetime.datetime.strptime(dob, "%d-%m-%Y").date()
+	print("DOB = ", a)
+	today = datetime.date.today()
+	print("Age = ", end="")
+	print(today.year - a.year)
+except:
+	print("Please enter a valid date and in the specified format")
